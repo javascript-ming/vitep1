dfb1895a88919d800f132ad8d5ddc4663f51d456
FRADY-9020M\frady <wujixmm@gmail.com>
2024-05-28 11:31:12 +0800

    fix: 路由问题修复

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,15 @@
+/*
+ * @Author: FRADY-9020M\frady wujixmm@gmail.com
+ * @Date: 2024-05-28 11:24:38
+ * @LastEditors: FRADY-9020M\frady wujixmm@gmail.com
+ * @LastEditTime: 2024-05-28 11:25:42
+ * @FilePath: \vitep1\src\router\index.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 import router from '../modules/router'
 
 router.beforeEach((to, from, next) => {
     // to and from are both route objects. must call `next`.
     console.log(to, from, next)
+    next()
 })
--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,11 @@
+/*
+ * @Author: FRADY-9020M\frady wujixmm@gmail.com
+ * @Date: 2024-05-28 11:24:33
+ * @LastEditors: FRADY-9020M\frady wujixmm@gmail.com
+ * @LastEditTime: 2024-05-28 11:29:04
+ * @FilePath: \vitep1\vite.config.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 import { defineConfig } from "vite"
 import Vue from "@vitejs/plugin-vue"
 import Components from 'unplugin-vue-components/vite'
@@ -12,7 +20,7 @@ export default defineConfig({
         AutoImport({
             imports:[ 'vue', 'vue-router', 'pinia']
         }),
-         Components({
+        Components({
         resolvers: [ElementPlusResolver()]
     }
     )]
